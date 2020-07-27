28137486ab614b9c0defabb0110c37c35d9a21b7
Patrick Tripp <patrick.tripp@rpsgroup.com>
2020-07-27 13:32:51 -0400

    Gfortran updates are in ROMS repository now.

--- SORC/checkout.sh
+++ SORC/checkout.sh
@@ -4,8 +4,6 @@ romsuser=patrickt
 svn checkout --username $romsuser https://www.myroms.org/svn/coupling/trunk ROMS
 
 cd ./ROMS
-aws s3 cp s3://ioos-cloud-sandbox/public/wrfroms/roms_coupling_gfort_fixes.tgz .
-tar -xvf roms_coupling_gfort_fixes.tgz
 cd ..
 
 # Note: If you encounter an error message while retrieving ROMS similar to the following:
@@ -20,4 +18,3 @@ git clone git@github.com:asascience/WRF.git
 cd WRF
 git checkout v4.1.3_cpl_gfort
 cd ..
-
